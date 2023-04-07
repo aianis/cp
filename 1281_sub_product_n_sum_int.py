758881242ddc66eddc20e8cf7056b0bc08b81cdb
Anis <anisjonberdiev77@gmail.com>
2023-04-07 23:55:42 +0800

    daily cp

--- 1281_sub_product_n_sum_int.py
+++ 1281_sub_product_n_sum_int.py
@@ -1,1 +1,48 @@
-1281_sub_product_n_sum_int.py
+"""Given an integer number n, return the difference between the product of its digits and the sum of its digits.
+ 
+
+Example 1:
+
+Input: n = 234
+Output: 15 
+Explanation: 
+Product of digits = 2 * 3 * 4 = 24 
+Sum of digits = 2 + 3 + 4 = 9 
+Result = 24 - 9 = 15"""
+
+#Approach: 
+# 1. We can use the modulo operator to get the last digit of the number.
+# 2. We can use the floor division operator to remove the last digit of the number.
+# 3. We can use the product and sum variables to store the product and sum of the digits.
+# 4. We can use a while loop to iterate until the number becomes 0.
+# 5. We can use the product and sum variables to store the product and sum of the digits.
+
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        product = 1
+        sum = 0
+        while n:
+            product *= n % 10
+            sum += n % 10
+            n = n // 10
+        return product - sum
+    
+
+
+#Time complexity: O(log n)  #as the input size grows, the number of operations also increases
+#Space complexity: O(1)  #constant space is used as the number of variables remain constant
+
+
+class Solution:
+    def subtractProductAndSum(self, n: int) -> int:
+        product = 1
+        sum = 0 
+        result = 0 
+        for num in str(n):
+            product *= int(num)
+            sum += int(num)
+        result = product - sum 
+        return result 
+
+        #Solution: easy to understand, but lil slower than than the previous one with O(n)
+
--- num_1_bit.py
+++ num_1_bit.py
@@ -1,1 +1,47 @@
-num_1_bit.py
+"""Write a function that takes the binary representation of an unsigned integer and returns the number of '1' bits it has (also known as the Hamming weight).
+
+Note:
+
+Note that in some languages, such as Java, there is no unsigned integer type. In this case, the input will be given as a signed integer type. It should not affect your implementation, as the integer's internal binary representation is the same, whether it is signed or unsigned.
+In Java, the compiler represents the signed integers using 2's complement notation. Therefore, in Example 3, the input represents the signed integer. -3.
+ 
+
+Example 1:
+
+Input: n = 00000000000000000000000000001011
+Output: 3
+Explanation: The input binary string 00000000000000000000000000001011 has a total of three '1' bits."""
+
+#Approach: Bitwise operations 
+
+#Explanation: 
+# 1. We can use the bitwise AND operator to check if the last bit is 1 or not.
+# 2. If the last bit is 1, we increment the count.
+# 3. We then right shift the number by 1 bit.
+# 4. We repeat the above steps until the number becomes 0.
+
+
+class Solution: 
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n:
+            if n & 1:
+                count += 1
+            n = n >> 1
+        return count
+    
+#Time complexity: O(log n)  #as the input size grows, the number of operations also increases
+#Space complexity: O(1)  #constant space is used as the number of variables remain constant
+
+
+#Faster solution
+class Solution:
+    def hammingWeight(self, n: int) -> int:
+        count = 0
+        while n:
+            count += 1
+            n = n & (n - 1)
+        return count
+    
+
+
